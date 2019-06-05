c0354363083c90183c3f1d966779468d85b6c03b
Pierre Bretéché <pbreteche@dawan.fr>
2019-06-05 14:33:56 +0200

    fournisseurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {InjectionToken, NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { BeerDetailComponent } from './beer-detail/beer-detail.component';
-import { BeerListComponent } from './beer-list/beer-list.component';
-import { BeerFormComponent } from './beer-form/beer-form.component';
-import { BeerReactiveFormComponent } from './beer-reactive-form/beer-reactive-form.component';
+import {AppComponent} from './app.component';
+import {BeerDetailComponent} from './beer-detail/beer-detail.component';
+import {BeerListComponent} from './beer-list/beer-list.component';
+import {BeerFormComponent} from './beer-form/beer-form.component';
+import {BeerReactiveFormComponent} from './beer-reactive-form/beer-reactive-form.component';
+import {CurrentBeerService} from './current-beer.service';
+import {BeerCategory} from '../beer';
+
+const currentBeerFactory = (beerName: string) => {
+  const service = new CurrentBeerService();
+  service.beer = { name: beerName, country: 'pays', alcoholByVolume: 6, category: BeerCategory.Lambic };
+  return service;
+};
+
+const DEFAULT_BEER_NAME = new InjectionToken('defaultBeerName');
 
 @NgModule({
   declarations: [
@@ -21,7 +31,10 @@ import { BeerReactiveFormComponent } from './beer-reactive-form/beer-reactive-fo
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: CurrentBeerService, useFactory: currentBeerFactory, deps: [DEFAULT_BEER_NAME] },
+    { provide: DEFAULT_BEER_NAME, useValue: 'Kro' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
